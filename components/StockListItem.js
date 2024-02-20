098532f2474dfb723a1a0ec356e6e20f53d8347a
Vinay Negi <vinegi004@gmail.com>
2024-02-20 22:23:21 +0530

    Create home screen

---
+++ components/StockListItem.js
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import AntDesign from '@expo/vector-icons/AntDesign';
+const StockListItem = ({stock}) => {
+
+
+  const change=Number.parseFloat(stock.percent_change).toFixed(1);  
+  return (
+    <View className="flex-row">
+    <View className="mb-4 p-1">
+    <View className="flex-row">
+        <Text className=" font-bold text-xl text-white">{stock.symbol}</Text>
+        <View className="justify-center items-center ml-1">
+        <AntDesign name="staro" size={18} color="white"/></View>
+    </View>
+    <View>
+        <Text className="text-white text-lg w-80">{stock.name}</Text>
+    </View>  
+    </View>
+    <View className="flex-1 mt-3 justify-start items-end w-24 ">
+        <Text className= " p-1 text-white">{Number.parseFloat(stock.close).toFixed(1)}</Text>
+        <Text className="  text-white p-1" style={{color:change > 0 ? "green" : "red"}}>{change > 0 ? "+": ""}{change}%</Text>
+    </View>
+    </View>
+  )
+}
+
+export default StockListItem
+
+const styles = StyleSheet.create({})
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, KeyboardAvoidingView, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import AntDesign from '@expo/vector-icons/Ionicons';
+import FontAwesome from '@expo/vector-icons/Ionicons';
+import top5 from '../data/top5.json'
+import StockListItem from '../components/StockListItem';
 
 const HomeScreen = () => {
+const stocks=Object.values(top5)
   return (
-    <View>
-      <Text className="text-red-500">HomeScreen</Text>
-    </View>
+    <KeyboardAvoidingView behavior='padding' className="rounded-sm">
+        <SafeAreaView className='h-full'>
+           
+            <View className="flex-1"> 
+               <FlatList data={stocks} 
+               className="mt-4"
+               renderItem={({item})=>(<StockListItem stock={item}/>)}/>
+                <View style={{backgroundColor:"#36454F"}}className="h-14 bg-gray-900 justify-center rounded-md items-center flex-row">
+                    <TouchableOpacity className=" p-4 mr-32">
+                    <AntDesign name="home" size={28} color="white"/>
+                    </TouchableOpacity>
+                    <TouchableOpacity className="p-4" onPress={()=>console.log(stocks)}>
+                    <FontAwesome name="list" size={28} color="white"/>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </SafeAreaView>
+    </KeyboardAvoidingView>
   )
 }
 
